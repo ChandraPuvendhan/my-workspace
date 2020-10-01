f249488ca43d187d3170b6259ff2579d3d58debe
ChandraPuvendhan <mailtochandrasankar@gmail.com>
2020-10-02 00:53:49 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+public static void main(String[] args){
+	System.out.println("Hello");
+}
+}
+class Welcome {
+public static void main(String[] args){
+	System.out.println("Welcome");
+}
+}
